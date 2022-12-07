e068cb2f8f3f5e45ec25820b90a9fe3efb656ece
Gomurmamma <dnaldyoung@gmail.com>
2022-12-07 14:44:40 -0500

    Added next.config, initialized pages, styles dir.

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+
+const path = require("path");
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+module.exports = nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage: React.FunctionComponent = () => {
+  return (
+    <>
+      <h1>Hello world!</h1>
+    </>
+  );
+};
+
+export default Homepage;
