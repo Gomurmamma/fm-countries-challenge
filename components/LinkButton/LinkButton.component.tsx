4c15fc7e1c99341da8a9e976a8d6752684336699
Gomurmamma <dnaldyoung@gmail.com>
2022-12-27 15:16:33 -0500

    hover state scale & pointer cursor, added optional tooltip prop

--- components/LinkButton/LinkButton.component.tsx
+++ components/LinkButton/LinkButton.component.tsx
@@ -5,13 +5,19 @@ type ButtonProps = {
   buttonprops: {
     title: string;
     html?: React.ReactElement;
+    tooltip?: string;
   };
   onClick?: () => void;
 };
 
 const LinkButton = ({ buttonprops, onClick }: ButtonProps) => {
   return (
-    <button type="button" className={style.LinkButton} onClick={onClick}>
+    <button
+      type="button"
+      className={style.LinkButton}
+      onClick={onClick}
+      title={buttonprops.tooltip}
+    >
       <>{buttonprops.html}</>
       {buttonprops.title.replace(/[a-z](?=[A-Z])/gm, "$& ")}
     </button>
