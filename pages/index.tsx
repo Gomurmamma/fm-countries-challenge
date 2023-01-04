fd5c575a8f4791e6e691457c73ea370ee7c279c5
Gomurmamma <dnaldyoung@gmail.com>
2023-01-04 16:42:21 -0500

    meta tags updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "../components/Header/Header.component";
 import SearchFeatures from "../components/SearchFeatures/SearchFeatures.component";
+import Head from "next/head";
 
 import dynamic from "next/dynamic";
 
@@ -26,6 +27,14 @@ const ToggleButton = dynamic(
 const Homepage: React.FunctionComponent = ({ countriesData }: Props) => {
   return (
     <>
+      <Head>
+        <title>REST Countries API Challenge - Tony Young</title>
+        <meta
+          name="description"
+          content="This is a solution to the Front End Mentors .io challenge. Contact Tony Young for more info."
+          key="desc"
+        ></meta>
+      </Head>
       <Header heading="Where in the World?">
         <ToggleButton />
       </Header>
